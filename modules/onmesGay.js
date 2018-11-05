b4b2a1adce59ce928a64654fda1b2f88df6585a5
iamtechi27 <iamtechi27@gmail.com>
2018-11-04 16:15:28 -0800

    updated "your gay" message checks and removed duplicate file

--- modules/onmesGay.js
+++ modules/onmesGay.js
@@ -5,7 +5,7 @@ const utils = require('../tbUtils.js');
 module.exports = {
 	name: 'gay',
 	execute(msg) {
-		if (/(u ?r|you'?re?) (g(a|e)y|gei)/.test(msg.content.toLowerCase())) {
+		if (/(u ?r|you'?re?) (g((ay|ae)|e)y|gei)/.test(msg.content.toLowerCase())) {
 			utils.timedReply(msg, 'no u');
 			return;
 		} else if (/i'?m gay/.test(msg.content)) {
