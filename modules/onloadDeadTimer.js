8b0354f726b5862e24a911317a887262908ad6b1
iamtechi27 <iamtechi27@gmail.com>
2018-11-10 13:46:12 -0800

    added test for dead timer to avoid spamming my console when i start up the bot in a dev environment

--- modules/onloadDeadTimer.js
+++ modules/onloadDeadTimer.js
@@ -7,11 +7,13 @@ module.exports = {
 	name: 'dead timer',
 	priority: 3,
 	onInit: async function(client) {
-		if (tbData.vars.lastRecvd < (Date.now() - 86400000)) {
-			client.guids.get(tbData.consts.randomOsuServer).channels.get(tbData.consts.randomOsuGeneral).send('Dead server.');
-			tbData.vars.lastRecvd = Date.now();
+		if (client.guilds.get(tbData.consts.randomOsuServer).available) {
+			if (tbData.vars.lastRecvd < (Date.now() - 86400000)) {
+				client.guilds.get(tbData.consts.randomOsuServer).channels.get(tbData.consts.randomOsuGeneral).send('Dead server.');
+				tbData.vars.lastRecvd = Date.now();
+			}
+			await utils.sleep(60000);
+			this.onInit();
 		}
-		await utils.sleep(60000);
-		this.onInit();
 	}
 }
