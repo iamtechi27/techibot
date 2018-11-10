d9ecefba68b7ceb5375290d1b04c2bde824e69b3
iamtechi27 <iamtechi27@gmail.com>
2018-11-10 12:05:40 -0800

    added ban functionality

--- modules/ongmaBanSpamURLs.js
+++ modules/ongmaBanSpamURLs.js
@@ -1,9 +1,16 @@
 //ongmaBanSpamURLs.js
 
+var tbData = require('../data/tbData.js');
+
 module.exports = {
 	name: 'ban spam urls',
 	description: 'checks nicknames of new members joining a guild and bans them if it contains some urls',
 	execute: function(member) {
+		for (var url in tbData.dictionary.get('bannedUsernameURLs')) {
+			if member.displayName.includes(url) {
+				member.ban('username contains banned url');
+			}
+		}
 		return;
 	}
 }
