3cabfc16fc058f4e5fa94a22ada4e3f433a5c1c7
iamtechi27 <iamtechi27@gmail.com>
2018-11-10 12:56:56 -0800

    removed some debug stuff

--- modules/ongmaBanSpamURLs.js
+++ modules/ongmaBanSpamURLs.js
@@ -6,13 +6,9 @@ module.exports = {
 	name: 'ban spam urls',
 	description: 'checks nicknames of new members joining a guild and bans them if it contains some urls',
 	execute: function(member) {
-		console.log('1');
 		var bannedUsernameURLs = tbData.dictionary.get('bannedUsernameURLs');
-		console.log('1.5');
 		for (var i in bannedUsernameURLs) {
-			console.log('2');
 			if (member.displayName.toLowerCase().includes(bannedUsernameURLs[i])) {
-				console.log('3');
 				member.ban('username contains banned url');
 				console.log(`banned ${member.displayName} on gma`);
 			}
