a61f992ba34ef8663162f1affe5c2a81599e5615
iamtechi27 <iamtechi27@gmail.com>
2018-11-10 18:44:40 -0800

    more spam riddening

--- modules/onloadDeadTimer.js
+++ modules/onloadDeadTimer.js
@@ -7,13 +7,15 @@ module.exports = {
 	name: 'dead timer',
 	priority: 3,
 	onInit: async function(client) {
-		if (client.guilds.get(tbData.consts.randomOsuServer).available) {
-			if (tbData.vars.lastRecvd < (Date.now() - 86400000)) {
-				client.guilds.get(tbData.consts.randomOsuServer).channels.get(tbData.consts.randomOsuGeneral).send('Dead server.');
-				tbData.vars.lastRecvd = Date.now();
+		try {
+			if (client.guilds.get(tbData.consts.randomOsuServer).available) {
+				if (tbData.vars.lastRecvd < (Date.now() - 86400000)) {
+					client.guilds.get(tbData.consts.randomOsuServer).channels.get(tbData.consts.randomOsuGeneral).send('Dead server.');
+					tbData.vars.lastRecvd = Date.now();
+				}
+				await utils.sleep(60000);
+				this.onInit();
 			}
-			await utils.sleep(60000);
-			this.onInit();
-		}
+		} catch (error) {}
 	}
 }
