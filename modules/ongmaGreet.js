0d85baec86ff4ebe6a54d6f8982660587f3daf75
iamtechi27 <iamtechi27@gmail.com>
2018-11-10 19:08:27 -0800

    finished up greet function NOW WE MERGE

--- modules/ongmaGreet.js
+++ modules/ongmaGreet.js
@@ -2,15 +2,24 @@
 
 const utils = require('../tbUtils.js');
 const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
 	name: 'greet',
 	description: 'greets new members upon joining guilds',
 	guilds: [],
 	guildGreetChannels: new Map(),
+	save: function() {
+		fs.writeFile("./data/greetGuilds.json", JSON.stringify(this.guilds), "utf8", (err) => {
+			if (err) throw err;
+		});
+		fs.writeFile("./data/greetGuildChannels.json", JSON.stringify(Array.from(this.guildGreetChannels)), "utf8", (err) => {
+			if (err) throw err;
+		});
+	},
 	onInit: function() {
-		this.guilds.push('324596059953430529');
-		this.guildGreetChannels.set('324596059953430529', '497183277900365835');
+		this.guilds = JSON.parse(fs.readFileSync('./data/greetGuilds.json', 'utf-8'));
+		this.guildGreetChannels = new Map(JSON.parse(fs.readFileSync('./data/greetGuildChannels.json', 'utf-8')));
 	},
 	execute: async function(member) {
 		if (this.guilds.includes(member.guild.id)) {
