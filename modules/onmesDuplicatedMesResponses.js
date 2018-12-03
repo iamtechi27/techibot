770640367ae687b553fa4374ed11245d3cc2a9f2
iamtechi27 <iamtechi27@gmail.com>
2018-12-02 19:04:47 -0800

    well try this i guess

--- modules/onmesDuplicatedMesResponses.js
+++ modules/onmesDuplicatedMesResponses.js
@@ -6,13 +6,14 @@ module.exports = {
 	name: 'DuplicateMesResponses',
 	execute(msg) {
 		
-		msg.channel.fetchMessages({ limit: 2 })
-			.then(messages => {
-				if ((Array.from(messages.values())[1].content.toLowerCase() == Array.from(messages.values())[0].content.toLowerCase()) && (Array.from(messages.values())[0].author.id != Array.from(messages.values())[1].author.id)) {
-					utils.timedReply(msg, Array.from(messages.values())[1].content);
-				}
-			})
-			.catch(console.error);
-		
+		if(msg.content != "") {
+			msg.channel.fetchMessages({ limit: 2 })
+				.then(messages => {
+					if ((Array.from(messages.values())[1].content.toLowerCase() == Array.from(messages.values())[0].content.toLowerCase()) && (Array.from(messages.values())[0].author.id != Array.from(messages.values())[1].author.id)) {
+						utils.timedReply(msg, Array.from(messages.values())[1].content);
+					}
+				})
+				.catch(console.error);
+		}
 	}
 }
