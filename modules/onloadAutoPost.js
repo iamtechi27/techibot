f751e6411e27e932e5752da843a69e2605fed72f
iamtechi27 <iamtechi27@gmail.com>
2018-12-05 16:11:59 -0800

    finished up autopost module, for now

--- modules/onloadAutoPost.js
+++ modules/onloadAutoPost.js
@@ -3,18 +3,30 @@
 var tbData = require('../data/tbData.js');
 const utils = require('../tbUtils.js');
 var fs = require('fs');
+var path = require('path');
+var Discord = require('discord.js');
 
 module.exports = {
 	name: 'auto post',
 	priority: 3,
 	onInit: async function(client) {
-		try {
-			if (!fs.existsSync('./autopost')) {
-				fs.mkdirSync('./autopost');
+		if (!fs.existsSync('./autopost')) {
+			fs.mkdirSync('./autopost');
+		}
+		
+		while (true) {
+			await utils.sleep(Math.floor(Math.random() * 7200000) + 3600000);
+			var autopostDirs = fs.readdirSync('./autopost').filter(file => fs.lstatSync(path.resolve('./autopost', file)).isDirectory());
+			for (var i in autopostDirs) {
+				if (Math.floor(Math.random() * 2)) {
+					try {
+						var autopostImages = fs.readdirSync('./autopost/' + autopostDirs[i]).filter(file => file.endsWith('.png') | file.endsWith('.jpg') | file.endsWith('.jpeg'));
+						var autopostImageIndex = Math.floor(Math.random() * autopostImages.length);
+						await client.channels.get(autopostDirs[i].substring(autopostDirs[i].indexOf('(') + 1, autopostDirs[i].indexOf(')'))).send(new Discord.Attachment('./autopost/' + autopostDirs[i] + '/' + autopostImages[autopostImageIndex]));
+						fs.unlinkSync('./autopost/' + autopostDirs[i] + '/' + autopostImages[autopostImageIndex]);
+					} catch (error) {}
+				}
 			}
-			//await utils.sleep(Math.floor(Math.random() * 7200000) + 3600000);
-			await utils.sleep(5000);
-			
-		} catch (error) {}
+		}
 	}
 }
