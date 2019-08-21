28ace23f1c409a7028b838e2b49bf4bc0bfb2fea
iamtechi27 <iamtechi27@gmail.com>
2019-08-20 19:39:28 -0700

    finished listservers command, maybe works?

--- commands/listservers.js
+++ commands/listservers.js
@@ -3,12 +3,12 @@
 module.exports = {
 	name: 'listservers',
 	description: 'Lists all servers that the bot is currently in.',
-	execute(msg) {
+	execute(msg, args, client) {
 		var guilds = client.guilds;
 		var response = "";
 		for (let [snowflake, guild] of guilds) {
-		response += guild.name + "(" + snowflake + ")\n";
-		}
+			response += guild.name + ' (' + snowflake + ')\n';
+			}
 		msg.channel.send(response)
 	},
 };
