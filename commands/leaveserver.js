2b41aefdeb03786a254d9897ff0119b587d8133d
iamtechi27 <iamtechi27@gmail.com>
2019-08-20 21:23:40 -0700

    added leave server command

---
+++ commands/leaveserver.js
@@ -0,0 +1,25 @@
+//leaveserver.js
+
+const utils = require('../tbUtils');
+var tbData = require('../data/tbData.js');
+
+module.exports = {
+	name: 'leaveserver',
+	description: 'Leaves the server specified by ID as an argument',
+	usage: 'server-id',
+	args: true,
+	execute(msg, args, client) {
+		if (msg.author.id != tbData.consts.techi) {
+			utils.timedReply(msg, 'nope!');
+			return;
+		}
+		var guildID = args.shift();
+		if (client.guilds.get(guildID).available) {
+			var guildName = client.guilds.get(guildID).name;
+			client.guilds.get(guildID).leave();
+			msg.reply('I left ' + guildName + '(' + guildID + ')');
+		} else {
+			msg.reply('couldn\'t find that server');
+		}
+	},
+};
