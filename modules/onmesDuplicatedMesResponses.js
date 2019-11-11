918f23f250591adda45fcf1131eb0181f2697fef
iamtechi27 <iamtechi27@gmail.com>
2019-11-10 16:36:46 -0800

    fixed stupid tard response triggering the dupe message response i should realy fix this in a less hack way

--- modules/onmesDuplicatedMesResponses.js
+++ modules/onmesDuplicatedMesResponses.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'DuplicateMesResponses',
 	execute(msg) {
 		
-		if(msg.content != "" && msg.content != "nya~!" && msg.content.toLowerCase() != "hoes mad") {
+		if(msg.content != "" && msg.content != "nya~!" && msg.content.toLowerCase() != "hoes mad" && msg.content.toLowerCase() != "how") {
 			msg.channel.fetchMessages({ limit: 2 })
 				.then(messages => {
 					if ((Array.from(messages.values())[1].content.toLowerCase() == Array.from(messages.values())[0].content.toLowerCase()) && (Array.from(messages.values())[0].author.id != Array.from(messages.values())[1].author.id)) {
